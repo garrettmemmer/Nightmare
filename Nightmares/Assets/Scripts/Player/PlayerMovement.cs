5925ecff0e97bfa91b145e96f98a3008f5492a04
Memmer <gtmemmer@ads.iu.edu>
2018-02-20 15:44:32 -0500

    Second video 20:56 player setup

--- Nightmares/Assets/Scripts/Player/PlayerMovement.cs
+++ Nightmares/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,18 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+	public float speed = 6f;
+
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	void Awake()
+	{
+		floorMask = LayerMask.GetMask ("Floor");
+		anim GetComponent <Animator> ();
+		playerRigidbody = GetComponent<playerRigidbody>();
+	}
 }
