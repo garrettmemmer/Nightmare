6a8c25cf4896ffe1420f237bffc2b376a6f251c3
Garrett Memmer <gtmemmer@indiana.edu>
2018-03-07 01:03:57 -0500

    toggle flashlight on/off

---
+++ Nightmares/Assets/FlighLightOnOff.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlighLightOnOff : MonoBehaviour {
+
+	public Light light;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (1)) {
+			light.enabled = !light.enabled;
+			print ("space pressed");
+		}
+	}
+}
