0cd4fdd5233b89e5f3dc5a145e413388103f55d9
Memmer <gtmemmer@ads.iu.edu>
2018-03-24 16:21:11 -0400

    tried to make a winning animation, couldnt get it

--- Nightmares/Assets/Scripts/Win.cs
+++ Nightmares/Assets/Scripts/Win.cs
@@ -5,12 +5,35 @@ using UnityEngine;
 public class Win : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+	bool playerInWin = false;
+	GameObject player;
+	Animator anim;
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject == player) {
+			playerInWin = true;
+
+
+			print("YOU WIN");
+		}
 	}
-	
+
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		anim = GetComponent<Animator>();
+
+	}
 	// Update is called once per frame
 	void Update () {
-		
+		if (playerInWin == true) {
+			anim.SetTrigger ("YouWin");
+			//play win screen
+			//			print("YOU WIN");
+		}
 	}
 }
+
--- Nightmares/Assets/Scripts/WinngZone.cs
+++ Nightmares/Assets/Scripts/WinngZone.cs
@@ -6,27 +6,24 @@ public class WinngZone : MonoBehaviour {
 
 	bool playerInWin = false;
 	GameObject player;
+	Animator anim;
 
-	// Use this for initialization
-	void Start () {
 
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-	}
 
 	void Awake ()
 	{
+
 		player = GameObject.FindGameObjectWithTag ("Player");
 	}
+
+
 
 	void OnTriggerEnter (Collider other)
 	{
 		if (other.gameObject == player) {
 			playerInWin = true;
-			//play win screen
+
+			
 			print("YOU WIN");
 		}
 	}
